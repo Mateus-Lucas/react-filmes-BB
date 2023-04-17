a7b4d434c951a7d6baf7021ba173d325657a9129
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-04-17 20:09:42 -0300

    adicionando cards

--- pages/filmes/index.js
+++ pages/filmes/index.js
@@ -1,11 +1,23 @@
 import Pagina from '@/components/Pagina'
-import apiDeputados from '@/services/apiDeputados'
 import apiFilmes from '@/services/apiFilmes'
 import React, { useEffect, useState } from 'react'
+import { Card } from 'react-bootstrap'
 
 const index = (props) => {
     return (
         <Pagina titulo="Filmes">
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
             {props.filmes.map(item => (
                 <p>{item.title}</p>
             ))}
